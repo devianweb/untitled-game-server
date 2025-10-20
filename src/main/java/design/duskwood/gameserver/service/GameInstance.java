a33c6c2030ae5e6e8da3e7e28383326c0db31303
carrcig <87659522+carrcig@users.noreply.github.com>
2025-10-20 16:29:44 +0100

    clears messages

--- src/main/java/design/duskwood/gameserver/service/GameInstance.java
+++ src/main/java/design/duskwood/gameserver/service/GameInstance.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.locks.LockSupport;
 
 @Data
 @RequiredArgsConstructor
@@ -99,6 +100,10 @@ public class GameInstance implements Runnable {
             }
           }
         });
+
+        if (!messages.isEmpty()) {
+          messages.clear();
+        }
 
         updateGame();
         ifMovingSendPositionUpdates();
